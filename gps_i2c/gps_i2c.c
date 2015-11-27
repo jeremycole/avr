a34bba95a45ed68e9384bbb907353cee3a327f47
Jeremy Cole <jeremy@jcole.us>
2015-11-27 11:00:49 -0800

    Update to new NMEA library, track new data
    Track all data from RMC, GGA, GSA, GSV sentences. Still only export
    time information via I2C. Future support for exporting more complete
    GPS data forthcoming.

--- gps_i2c/gps_i2c.c
+++ gps_i2c/gps_i2c.c
@@ -1,9 +1,21 @@
 /*
- * i2c_gps.c
- *
- *  Created on: Dec 27, 2012
- *      Author: jcole
- */
+    Copyright (c) 2015, Jeremy Cole <jeremy@jcole.us>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,6 +31,12 @@
 #include <rtc.h>
 #include <nmea.h>
 
+//#define NMEA_DEBUG_SENTENCES
+//#define NMEA_DEBUG_RMC
+//#define NMEA_DEBUG_GGA
+//#define NMEA_DEBUG_GSA
+//#define NMEA_DEBUG_GSV
+
 uart_t *u0;
 uart_t *u1;
 rtc_datetime_24h_t current_dt;
@@ -27,6 +45,16 @@ uint8_t gps_uart_data_ready = 0;
 uint8_t nmea_sentence_ready = 0;
 char nmea_sentence[128];
 char *nmea_sentence_p = nmea_sentence;
+
+typedef struct _gps_state_t
+{
+  nmea_gprmc_t gprmc;
+  nmea_gpgga_t gpgga;
+  nmea_gpgsa_t gpgsa;
+  nmea_gpgsv_t gpgsv[4];
+} gps_state_t;
+
+gps_state_t gps_state;
 
 #define I2C_WAIT_CLEAR(v, b)  while(!((v) & _BV((b))))
 #define I2C_WAIT_SET(v, b)    while((v) & _BV((b)))
@@ -61,40 +89,235 @@ uint8_t handle_i2c_slave_rx(uint8_t status, i2c_mode_t last_mode, i2c_mode_t cur
   return 0;
 }
 
-#define NMEA_DISCARD (NMEA_INVALID_DATE | NMEA_INVALID_TIME)
-void handle_nmea_sentence(void)
+void print_date(nmea_date_t *date)
 {
+  printf("%04d-%02d-%02d",
+    date->year,
+    date->month,
+    date->day
+  );
+}
+
+void print_time(nmea_time_t *time)
+{
+  printf("%02d:%02d:%02d.%03d",
+    time->hour,
+    time->minute,
+    time->second,
+    time->millisecond
+  );
+}
+
+void print_nmea_gprmc(nmea_gprmc_t *gprmc)
+{
+  printf("NMEA RMC:\n");
+  printf("  Status: %c\n", gprmc->status);
+  printf("  Mode: %c\n", gprmc->mode);
+  printf("  Time: ");
+  print_date(&gprmc->date);
+  printf(" ");
+  print_time(&gprmc->time);
+  printf("\n");
+  printf("  Position: %f, %f\n",
+      gprmc->position.latitude,
+      gprmc->position.longitude);
+  printf("  Velocity: %f, %f\n",
+      gprmc->velocity.speed,
+      gprmc->velocity.heading);
+  printf("  Checksum: %02x\n", gprmc->checksum);
+  printf("\n");
+}
+
+// $GPRMC,070812.000,A,3923.1196,N,11937.6931,W,0.09,283.05,231115,,,A*74
+void handle_nmea_gprmc(char *sentence)
+{
+  unsigned int invalidity;
   nmea_gprmc_t gprmc;
-  unsigned int invalid;
 
-  if(strncmp_P(nmea_sentence, PSTR("$GPRMC,"), 7) != 0)
-    return;
-
-  printf("NMEA: %s\n", nmea_sentence);
-  if((invalid = nmea_parse_gprmc(nmea_sentence, &gprmc)) & NMEA_DISCARD)
+  invalidity = nmea_parse_gprmc(sentence, &gprmc);
+  if(invalidity != 0)
   {
-    printf("Invalid NMEA: %04x\n", invalid);
-    //return;
+    printf("NMEA RMC Invalid: %04x; Checksum: %02x\n", invalidity, gprmc.checksum);
   }
 
-  printf("NMEA Time: %04d-%02d-%02d %02d:%02d:%02d\n",
-    gprmc.year,
-    gprmc.month,
-    gprmc.date,
-    gprmc.hour,
-    gprmc.minute,
-    gprmc.second
-  );
+  gps_state.gprmc = gprmc;
 
-  current_dt.year   = gprmc.year;
-  current_dt.month  = gprmc.month;
-  current_dt.date   = gprmc.date;
-  current_dt.hour   = gprmc.hour;
-  current_dt.minute = gprmc.minute;
-  current_dt.second = gprmc.second;
+#ifdef NMEA_DEBUG_RMC
+  print_nmea_gprmc(&gprmc);
+#endif // NMEA_DEBUG_RMC
+
+  if(invalidity & (NMEA_INVALID_DATE | NMEA_INVALID_TIME))
+    return;
+
+  current_dt.year   = gprmc.date.year;
+  current_dt.month  = gprmc.date.month;
+  current_dt.date   = gprmc.date.day;
+  current_dt.hour   = gprmc.time.hour;
+  current_dt.minute = gprmc.time.minute;
+  current_dt.second = gprmc.time.second;
   current_dt.day_of_week =
-      rtc_find_dow(gprmc.year, gprmc.month, gprmc.date);
+      rtc_find_dow(gprmc.date.year, gprmc.date.month, gprmc.date.day);
+}
 
+void print_nmea_gpgga(nmea_gpgga_t *gpgga)
+{
+  printf("NMEA GGA:\n");
+  printf("  Time: ");
+  print_time(&gpgga->time);
+  printf("\n");
+  printf("  Position: %f, %f\n",
+      gpgga->position.latitude,
+      gpgga->position.longitude);
+  printf("  Fix Quality: %d\n", gpgga->fix_quality);
+  printf("  Satellites Tracked: %d\n", gpgga->satellites_tracked);
+  printf("  HDOP: %f\n", gpgga->hdop);
+  printf("  Altitude: %f\n", gpgga->altitude);
+  printf("  Geoid Distance: %f\n", gpgga->geoid_height);
+  printf("  Checksum: %02x\n", gpgga->checksum);
+  printf("\n");
+}
+
+// $GPGGA,070812.000,3923.1196,N,11937.6931,W,1,10,0.81,1773.2,M,-21.2,M,,*62
+void handle_nmea_gpgga(char *sentence)
+{
+  unsigned int invalidity;
+  nmea_gpgga_t gpgga;
+
+  invalidity = nmea_parse_gpgga(sentence, &gpgga);
+  if(invalidity != 0)
+  {
+    printf("NMEA GGA Invalid: %04x; Checksum: %02x\n", invalidity, gpgga.checksum);
+  }
+
+  gps_state.gpgga = gpgga;
+
+#ifdef NMEA_DEBUG_GGA
+  print_nmea_gpgga(&gpgga);
+#endif // NMEA_DEBUG_GGA
+}
+
+void print_nmea_gpgsa(nmea_gpgsa_t *gpgsa)
+{
+  printf("NMEA GSA:\n");
+  printf("  Mode: %c\n", gpgsa->mode);
+  printf("  Fix Type: %c\n", gpgsa->fix_type);
+  printf("  Satellite PRNs:");
+  for(int index=0; index<12; index++)
+    printf(" %d", gpgsa->satellite_prn[index]);
+  printf("\n");
+  printf("  PDOP: %f\n", gpgsa->pdop);
+  printf("  HDOP: %f\n", gpgsa->hdop);
+  printf("  VDOP: %f\n", gpgsa->vdop);
+  printf("  Checksum: %02x\n", gpgsa->checksum);
+  printf("\n");
+}
+
+// $GPGSA,A,3,28,09,08,13,19,30,07,27,11,05,,,1.13,0.81,0.79*03
+void handle_nmea_gpgsa(char *sentence)
+{
+  unsigned int invalidity;
+  nmea_gpgsa_t gpgsa;
+
+  invalidity = nmea_parse_gpgsa(sentence, &gpgsa);
+  if(invalidity != 0)
+  {
+    printf("NMEA GSA Invalid: %04x; Checksum: %02x\n", invalidity, gpgsa.checksum);
+  }
+
+  gps_state.gpgsa = gpgsa;
+
+#ifdef NMEA_DEBUG_GSA
+  print_nmea_gpgsa(&gpgsa);
+#endif // NMEA_DEBUG_GSA
+}
+
+void print_nmea_gpgsv(nmea_gpgsv_t *gpgsv)
+{
+  printf("NMEA GSV:\n");
+  printf("  Sentence: %d of %d\n", gpgsv->sentence_number, gpgsv->sentence_total);
+  printf("  Satellites in view (of %d):\n", gpgsv->satellites_in_view);
+  for(int i=0; i<4; i++)
+  {
+    if(gpgsv->satellite[i].prn)
+    {
+      printf("    %2d: PRN: %2d, Alt: %2d, Az: %3d, SNR: %2d\n",
+        gpgsv->satellite[i].index,
+        gpgsv->satellite[i].prn,
+        gpgsv->satellite[i].altitude,
+        gpgsv->satellite[i].azimuth,
+        gpgsv->satellite[i].snr);
+    }
+  }
+  printf("  Checksum: %02x\n", gpgsv->checksum);
+  printf("\n");
+}
+
+void print_nmea_gpgsv_summary(nmea_gpgsv_t *gpgsv)
+{
+  printf("Satellites in view (%d):\n", gpgsv[0].satellites_in_view);
+  for(int s=0; s<gpgsv[0].sentence_total; s++)
+  {
+    if(gpgsv[s].satellites_in_view > 0)
+    {
+      for(int i=0; i<4; i++)
+      {
+        nmea_gpgsv_satellite_t *satellite = &gpgsv[s].satellite[i];
+        if(satellite->prn)
+        {
+          printf("  %2d: PRN: %2d, Alt: %2d, Az: %3d, SNR: %2d\n",
+            satellite->index,
+            satellite->prn,
+            satellite->altitude,
+            satellite->azimuth,
+            satellite->snr);
+        }
+      }
+    }
+  }
+  printf("\n");
+}
+
+// $GPGSV,4,1,13,07,66,049,21,30,62,322,20,28,48,239,23,09,41,161,22*74
+void handle_nmea_gpgsv(char *sentence)
+{
+  unsigned int invalidity;
+  nmea_gpgsv_t gpgsv;
+
+  invalidity = nmea_parse_gpgsv(sentence, &gpgsv);
+  if(invalidity != 0)
+  {
+    printf("NMEA GSV Invalid: %04x; Checksum: %02x\n", invalidity, gpgsv.checksum);
+  }
+
+  gps_state.gpgsv[gpgsv.sentence_number - 1] = gpgsv;
+
+#ifdef NMEA_DEBUG_GSV
+  print_nmea_gpgsv(&gpgsv);
+#endif // NMEA_DEBUG_GSV
+}
+
+void handle_nmea_sentence(void)
+{
+#ifdef NMEA_DEBUG_SENTENCES
+  printf("NMEA: %s", nmea_sentence);
+#endif // NMEA_DEBUG_SENTENCES
+
+  if(strncmp_P(nmea_sentence, PSTR("$GPRMC,"), 7) == 0)
+  {
+    handle_nmea_gprmc(nmea_sentence);
+  }
+  else if(strncmp_P(nmea_sentence, PSTR("$GPGGA,"), 7) == 0)
+  {
+    handle_nmea_gpgga(nmea_sentence);
+  }
+  else if(strncmp_P(nmea_sentence, PSTR("$GPGSA,"), 7) == 0)
+  {
+    handle_nmea_gpgsa(nmea_sentence);
+  }
+  else if(strncmp_P(nmea_sentence, PSTR("$GPGSV,"), 7) == 0)
+  {
+    handle_nmea_gpgsv(nmea_sentence);
+  }
 }
 
 void handle_gps_uart_input(uart_t *uart)
@@ -109,7 +332,7 @@ void handle_gps_uart_parsing(uart_t *uart)
   {
     if(nmea_sentence_p >= nmea_sentence+127)
     {
-      uart_puts(u0, "Resetting nmea_sentence due to overflow\n");
+      uart_puts(u0, "Resetting nmea_sentence due to overflow\r\n");
       *nmea_sentence = 0;
       nmea_sentence_p = nmea_sentence;
     }
@@ -126,15 +349,26 @@ void handle_gps_uart_parsing(uart_t *uart)
         nmea_sentence_p = nmea_sentence;
       }
     }
-    if(data & UART_BUFFER_OVERFLOW) uart_puts(u0, "UART_BUFFER_OVERFLOW\n");
-    if(data & UART_OVERRUN_ERROR) uart_puts(u0, "UART_OVERRUN_ERROR\n");
-    if(data & UART_FRAME_ERROR) uart_puts(u0, "UART_FRAME_ERROR\n");
+    //if(data & UART_BUFFER_OVERFLOW) uart_puts(u0, "UART_BUFFER_OVERFLOW\r\n");
+    //if(data & UART_OVERRUN_ERROR) uart_puts(u0, "UART_OVERRUN_ERROR\r\n");
+    //if(data & UART_FRAME_ERROR) uart_puts(u0, "UART_FRAME_ERROR\r\n");
   }
+}
+
+void print_gps_state(void)
+{
+  print_nmea_gprmc(&gps_state.gprmc);
+  print_nmea_gpgga(&gps_state.gpgga);
+  print_nmea_gpgsa(&gps_state.gpgsa);
+  print_nmea_gpgsv_summary(gps_state.gpgsv);
 }
 
 int main(void)
 {
+  char gps_state_printed = 0;
   _delay_ms(1000);
+
+  memset(&gps_state, 0, sizeof(gps_state));
 
   u0 = uart_init("0", UART_BAUD_SELECT(38400, F_CPU));
   uart_init_stdout(u0);
@@ -155,21 +389,21 @@ int main(void)
 
   while(1)
   {
-    //uart_puts(u0, "Test on UART 0!\r\n");
-    //printf("Bytes available on UART 0: %d\n", uart_data_ready(u0));
-    //printf("Bytes available on UART 1: %d\n", uart_data_ready(u1));
-    //uart_puts(u1, "Test on UART 1!\r\n");
-    /*
-    if(uart_data_ready(u1))
-    {
-      while((c = uart_getc(u1)) != UART_NO_DATA)
-        uart_putc(u0, c);
-    }
-    */
     handle_gps_uart_parsing(u1);
 
-    //_delay_ms(10);
+    if(gps_state.gprmc.time.second % 10 == 0)
+    {
+      if(gps_state_printed == 0)
+      {
+        print_gps_state();
+        gps_state_printed = 1;
+      }
+    }
+    else
+    {
+      gps_state_printed = 0;
+    }
   }
 
-  return(0);
+  return 0;
 }
